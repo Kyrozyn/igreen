5c476d6b0dcf3cc5c2f5e368a26160a453ca2153
kyrozyn <andreasrr998@gmail.com>
2022-02-13 21:43:53 +0700

    roles and migration

---
+++ database/migrations/2022_02_13_120312_create_laporans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laporans', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_laporan');
+            $table->string('name');
+            $table->enum('type',['image','video','imagevideo','text','option']);
+            $table->unsignedBigInteger('parent_menu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laporans');
+    }
+};
