508eaf103224c0da24af5f42067564741f06bf29
kyrozyn <andreasrr998@gmail.com>
2022-03-04 23:31:19 +0700

    revisi

--- database/migrations/2022_02_23_141802_create_front_menus_table.php
+++ database/migrations/2022_02_23_141802_create_front_menus_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('icon_url')->nullable();
-            $table->unsignedBigInteger('roles_id')->nullable();
             $table->timestamps();
         });
     }
