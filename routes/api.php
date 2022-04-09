5f2d8eff0843d52cfd5181348c0afc6fca9ed32d
Andreas <arizkirizaldi@mahasiswa.unikom.ac.id>
2022-04-09 12:58:58 +0700

    move routes

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/login',[\App\Http\Controllers\Api::class,'checkAccount']);
-
-Route::get('/getfrontmenu',[\App\Http\Controllers\Api::class,'getFrontMenu']);
-Route::post('/getmenu',[\App\Http\Controllers\Api::class,'getMenu']);
-Route::post('/getchildmenu',[\App\Http\Controllers\Api::class,'getChildMenu']);
-Route::post('/getlaporanmenu',[\App\Http\Controllers\Api::class,'getLaporanMenu']);
-
-Route::post('/postLaporan',[\App\Http\Controllers\Api::class,'postLaporan']);
-
-Route::post('/createPelaporan',[\App\Http\Controllers\Api::class,'createPelaporan']);
-Route::post('/laporanBatch',[\App\Http\Controllers\Api::class,'laporanBatch']);
-
-Route::post('/getLaporan',[\App\Http\Controllers\Api::class,'getLaporan']);
