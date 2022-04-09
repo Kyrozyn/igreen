7192e6b3ae0da3e28cce0eb79e8506562fbbff68
Andreas <arizkirizaldi@mahasiswa.unikom.ac.id>
2022-04-09 13:56:00 +0700

    structure

--- database/migrations/2022_04_06_170312_create_pelaporans_table.php
+++ database/migrations/2022_04_06_170312_create_pelaporans_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('pelaporans', function (Blueprint $table) {
             $table->id();
-            $table->enum("status",["Dibuka","Ditutup","Perlu Perbaikan"]);
+            $table->enum("status",["Diajukan","Menunggu Konfirmasi","Revisi","Selesai"]);
+            $table->enum("status_detail",["Sedang direview oleh Manager","Sedang direview oleh KTT", "[Manager] Perlu Perbaikan", "[KTT] Perlu Perbaikan", "Selesai"]);
             $table->foreignIdFor(\App\Models\User::class);
             $table->timestamps();
         });
